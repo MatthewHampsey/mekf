38940f314918caefa1f1397218fc4cd1b93cd9bc
MatthewHampsey <matthewjohnhampsey@gmail.com>
2020-07-30 23:36:54 +1000

    Corrections for process covariance

--- kalman2.py
+++ kalman2.py
@@ -14,17 +14,42 @@ from util import skewSymmetric, quatToMatrix
 # [12:15] accelerometer bias
 class Kalman:
 
-    def __init__(self, initial_est, estimate_covariance, process_covariance, observation_covariance):
+    def __init__(self, initial_est, estimate_covariance, 
+                       gyro_cov, gyro_bias_cov, accel_proc_cov, 
+                       accel_bias_cov, accel_obs_cov):
         self.estimate = initial_est
         self.estimate_covariance = estimate_covariance*np.identity(15, dtype=float)
-        self.process_covariance = process_covariance*np.identity(15, dtype=float)
-        self.observation_covariance = observation_covariance*np.identity(3, dtype=float)
+
+        self.observation_covariance = accel_obs_cov*np.identity(3, dtype=float)
         self.gyro_bias = np.array([0.0, 0.0, 0.0])
         self.accelerometer_bias = np.array([0.0, 0.0, 0.0])
 
         self.G = np.zeros(shape=(15, 15), dtype=float)
         self.G[0:3, 9:12] = -np.identity(3)
         self.G[6:9, 3:6] =  np.identity(3)
+
+        self.gyro_cov_mat = gyro_cov*np.identity(3, dtype=float)
+        self.gyro_bias_cov_mat = gyro_bias_cov*np.identity(3, dtype=float)
+        self.accel_cov_mat = accel_proc_cov*np.identity(3, dtype=float)
+        self.accel_bias_cov_mat = accel_bias_cov*np.identity(3, dtype=float)
+
+    def process_covariance(self, time_delta):
+        Q = np.zeros(shape=(15, 15), dtype=float)
+        Q[0:3, 0:3] = self.gyro_cov_mat*time_delta + self.gyro_bias_cov_mat*(time_delta**3)/3.0
+        Q[0:3, 9:12] = -self.gyro_bias_cov_mat*(time_delta**2)/2.0
+        Q[3:6, 3:6] = self.accel_cov_mat*time_delta + self.accel_bias_cov_mat*(time_delta**3)/3.0
+        Q[3:6, 6:9] = self.accel_bias_cov_mat*(time_delta**4)/8.0 + self.accel_cov_mat*(time_delta**2)/2.0
+        Q[3:6, 12:15] = -self.accel_bias_cov_mat*(time_delta**2)/2.0
+        Q[6:9, 3:6] = self.accel_cov_mat*(time_delta**2)/2.0 + self.accel_bias_cov_mat*(time_delta**4)/8.0
+        Q[6:9, 6:9] = self.accel_cov_mat*(time_delta**3)/3.0 + self.accel_bias_cov_mat*(time_delta**5)/20.0
+        Q[6:9, 12:15] = -self.accel_bias_cov_mat*(time_delta**3)/6.0
+        Q[9:12, 0:3] = -self.gyro_bias_cov_mat*(time_delta**2)/2.0
+        Q[9:12, 9:12] = self.gyro_bias_cov_mat*time_delta
+        Q[12:15, 3:6] = -self.accel_bias_cov_mat*(time_delta**2)/2.0
+        Q[12:15, 6:9] = -self.accel_bias_cov_mat*(time_delta**3)/6.0
+        Q[12:15, 12:15] = self.accel_bias_cov_mat*time_delta
+
+        return Q
         
     def update(self, gyro_meas, acc_meas, time_delta):
         
@@ -42,7 +67,7 @@ class Kalman:
         F = np.identity(15, dtype=float) + self.G*time_delta
 
         #Update with a priori covariance
-        self.estimate_covariance = np.dot(np.dot(F, self.estimate_covariance), F.transpose()) + self.process_covariance
+        self.estimate_covariance = np.dot(np.dot(F, self.estimate_covariance), F.transpose()) + self.process_covariance(time_delta)
 
         #Form Kalman gain
         H = np.zeros(shape=(3,15), dtype=float)
